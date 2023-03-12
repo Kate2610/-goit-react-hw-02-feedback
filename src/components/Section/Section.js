6bc05e3786c311c7b4d3d4b072490be5cd48bc36
Kateryna Anisimova <110602507+Kate2610@users.noreply.github.com>
2023-03-12 15:10:36 +0000

    Update Section.js

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -3,15 +3,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Section = ({ title, children }) => {
-	return (
-		<>
-			<h2>{title}</h2>
-			{children}
-		</>
-	);
+  return (
+    <>
+      <h2>{title}</h2>
+      {children}
+    </>
+  );
 };
 
 Section.propTypes = {
-	title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired
 };
+
 export default Section;
